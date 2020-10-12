098f63143717408bf3a0b651c2f1d1f7a1790289
bmichalczuk <barmic90@hotmail.com>
2020-10-12 23:41:08 +0200

    refactor styles for AppHeader, change paddings for auth link

--- client/src/containers/AppHeader/AppHeader.js
+++ client/src/containers/AppHeader/AppHeader.js
@@ -6,6 +6,7 @@ import Spinner from "../../components/Spinner/Spinner";
 import LoginIcon from "../../Icons/login";
 import LogoutIcon from "../../Icons/logout";
 import VisuallyInvisible from "../../shared/VisuallyHidden";
+import VisuallyHidden from "../../shared/VisuallyHidden";
 
 
 export const AppHeader = (props) => {
@@ -14,25 +15,29 @@ export const AppHeader = (props) => {
             case null:
                 return <Spinner />;
             case false: 
-                return (<a title="Login with Google" href="auth/google">
-                        <VisuallyInvisible>Login with Google</VisuallyInvisible> 
-                        <LoginIcon />
-                    </a>);
-            default: return (<a title="Logout" href="/api/logout">
-                    <VisuallyInvisible>Logout</VisuallyInvisible> 
-                    <LogoutIcon />
-                </a>);
+                return (
+                        <a className="auth__link" title="Login with Google" href="auth/google">
+                            <VisuallyInvisible>Login with Google</VisuallyInvisible> 
+                            <LoginIcon />
+                        </a>
+                    );
+            default: return (
+                    <a className="auth__link" title="Logout" href="/api/logout">
+                        <VisuallyInvisible>Logout</VisuallyInvisible> 
+                        <LogoutIcon />
+                    </a>
+                );
         }   
     }
     return (
         <header className={props.className}>
-            <h1>
+            <h1 className="heading">
                 <a href={props.auth ? "/diary" : "/"} title="DDiary">
                     <img src={ddiarylogo} alt="" />
-                    <span>DDiary</span>
+                    <VisuallyHidden>DDiary</VisuallyHidden>
                 </a>
             </h1>
-            <div>
+            <div className="auth">
                 {renderAuthStatus() }
             </div>
         </header>
@@ -46,7 +51,7 @@ const styledAppHeader = styled(AppHeader)`
     display: flex;
     justify-content: space-between;
     align-content: stretch;
-    background-color: ${props => props.theme.secondaryColor};
+    background-color: ${({theme: {secondaryColor}}) => secondaryColor};
     @media(min-width: ${({theme: {breakpoint}}) => breakpoint.medium}) {
         padding: .5em 5em;
     }
@@ -56,9 +61,8 @@ const styledAppHeader = styled(AppHeader)`
         fill: #ffffff;
         height: 1em;
         width: auto;
-        margin-left: 3px;
     }
-    h1 {
+    .heading {
         margin-left: 1em;
         display: flex;
         align-items: center;
@@ -67,32 +71,24 @@ const styledAppHeader = styled(AppHeader)`
             width: auto;
         }
     }
-    span {
-        position: absolute; 
-        overflow: hidden; 
-        clip: rect(0 0 0 0); 
-        height: 1px; width: 1px; 
-        margin: -1px; padding: 0; border: 0; 
-    }
-    div {
+    .auth {
         display: flex;
         justify-content: center;
         align-items: center;
-        a { 
-            padding: 0 1em;
+        .auth__link { 
             display: flex;
             align-items: center;
-            height: 100%;
+            justify-content: center;
             text-decoration: none;
             font-size: 1.5em;
             font-weight: bolder;
-            color: ${props => props.theme.primaryColor};
+            color: ${({theme: {primaryColor}}) => primaryColor};
             transition: .4s;
-            padding-right: .5em;
+            padding: .5em;
             :hover,
             :focus {
-                background-color: ${props => props.theme.fourthColor};
-                color: ${props => props.theme.secondaryColor};
+                background-color: ${({theme: {fourthColor}}) => fourthColor};
+                color: ${({theme: {secondaryColor}}) => secondaryColor};
             }
         }
     }
